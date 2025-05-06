313e43babf623a913e908951de59c6a92d4aa1e2
Jiang Yiwei <479772069@qq.com>
2025-05-06 00:53:28 -0500

    update the apptainer

--- utilization_profiling/llama2_ft/run.sh
+++ utilization_profiling/llama2_ft/run.sh
@@ -4,6 +4,6 @@
 SIF_IMAGE="docker://austinguish259/llama2:latest"
 CURRENT_DIR=$(pwd)
 NCU_BASE="ncu --target-processes all --profile-from-start no -f --metrics gpu__dram_throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.sum,sm__throughput.avg.pct_of_peak_sustained_elapsed --csv --devices 0"
-$NCU_BASE -o llama2_ft.csv tune run lora_finetune_single_device --config ./config.yaml epochs=1
-
+CMD=$NCU_BASE -o llama2_ft.csv tune run lora_finetune_single_device --config ./config.yaml epochs=1
+apptainer exec --nv $SIF_IMAGE bash -c "$CMD"
 #echo "Llama2 fine-tuning run completed. Output CSV can be found in ./reax_benchmark/ directory."
--- utilization_profiling/m-psdns/run.sh
+++ utilization_profiling/m-psdns/run.sh
@@ -4,6 +4,6 @@
 SIF_IMAGE="docker://austinguish259/m-psdns:latest"
 CURRENT_DIR=$(pwd)
 NCU_BASE="ncu --target-processes all -f --metrics gpu__dram_throughput.avg.pct_of_peak_sustained_elapsed,gpu__time_duration.sum,sm__throughput.avg.pct_of_peak_sustained_elapsed --csv --devices 0"
-$NCU_BASE -o m-psdns.csv DNS_PEN_GPU_p4.x -i $CURRENT_DIR/../../datasets/m-psdns/input
-
+CMD=$NCU_BASE -o m-psdns.csv DNS_PEN_GPU_p4.x -i $CURRENT_DIR/../../datasets/m-psdns/input
+apptainer exec --nv $SIF_IMAGE bash -c "$CMD"
 echo "m-psdns run completed
